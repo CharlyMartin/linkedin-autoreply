ca964890cb93b5539c31daf7e93f26d37b9726c9
Charly MARTIN <charly.martin.1207@gmail.com>
2017-08-18 10:52:40 +1000

    background.js acts as a router depending on events

---
+++ background.js
@@ -0,0 +1,21 @@
+// Start on LinkedIn Home Page
+
+// if ul.mn-invitation-list is null, then it means all the invitations have been
+// accepted, so stop. If not, starts.
+
+
+// //Open /mynetwork on a new tab || /mynetwork/invitation-manager/
+// "ul.mn-invitation-list":first-child -> first list item
+// Click on accept "button[data-control-name="accept"]"
+
+// //Button of the first item of the list
+// var acceptBtn = document.querySelector('ul.mn-invitation-list > li:first-child button[data-control-name="accept"]')
+// acceptBtn.click();
+// // Pop-up container
+// // div#ember887.ember-view
+
+// // Card with name and button
+// #ember1610 > div > div
+
+// // Button "Message" (thing to click)
+// #ember1610 > div > div > p > button

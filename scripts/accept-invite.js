b49d3dd0ff6db45b8ef97d78d3264133593b0892
Charly MARTIN <charly.martin.1207@gmail.com>
2017-08-21 09:47:02 +1000

    manifest.js injects script on invitation page

--- scripts/accept-invite.js
+++ scripts/accept-invite.js
@@ -1,18 +1,18 @@
 // Variables
-const inviteList = document.querySelector('ul.mn-invitation-list')
-const acceptBtn = inviteList.querySelector('li:first-child button[data-control-name="accept"]')
+// const inviteList = document.querySelector('ul.mn-invitation-list')
+// const acceptBtn = inviteList.querySelector('li:first-child button[data-control-name="accept"]')
 
 
 
-function acceptInvite() {
-  acceptBtn.click()
-};
+// function acceptInvite() {
+//   acceptBtn.click()
+// };
 
-function run(argument) {
-  acceptInvite if pending
-}
+// function run(argument) {
+//   acceptInvite if pending
+// }
 
-// Events
-document.addEventListener('pageContentLoaded', run)
+// // Events
+// document.addEventListener('pageContentLoaded', run)
 
-// ...
+console.log("Hello world!")

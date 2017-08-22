bbd4f95b6262fa3512673b1a0877b2760783ac2d
Charly MARTIN <charly.martin.1207@gmail.com>
2017-08-22 11:30:35 +1000

    send message WIP. click on button pb

--- scripts/send-message.js
+++ scripts/send-message.js
@@ -15,33 +15,37 @@ function buildMessageWith(name) {
   console.log(arguments.callee.name);
 
   const first = name.first;
-  const welcomeMessage = `Hello ${first}, happy to meet you!`;
+  const welcomeMessage = `Hello ${first}, happy to meet you 🙂`;
   return welcomeMessage;
 };
 
 function fillBoxWith(message) {
   console.log(arguments.callee.name);
 
+  document.querySelector("textarea.msg-compose-form__message-text").value = message;
+  document.querySelector("textarea.msg-compose-form__message-text").blur();
+  document.querySelector("textarea.msg-compose-form__message-text").focus();
 };
 
 function clickSendBtn() {
   console.log(arguments.callee.name);
 
+  document.querySelector("button.msg-compose-form__send-button").click();
 };
 
-function init() {
+function run() {
   console.log(arguments.callee.name);
 
   const name = extractName();
   const message = buildMessageWith(name);
-  // fillBoxWith(message);
-  // clickSendBtn;
+  fillBoxWith(message);
+  // Need to wait little time before beling able to click on button.
+  // Need to figure out which event fires the disable false on button.
+  // window.setTimeout(clickSendBtn, 500);
 };
 
 // Running the script
-
-// Fill the text area with given text
-// document.querySelector("textarea.msg-compose-form__message-text").value = "Text to paste"
+window.setTimeout(run, 2000);
 
 
 // Send the message
